08ebcd8b734c2e05ee0eea107f9d727e876a795a
mant1c0r3 <thelevimatusproject@gmail.com>
2016-03-14 20:31:42 -0300

    Tests outputs with Deck and Card classes

---
+++ src/blackjack/Test.java
@@ -0,0 +1,19 @@
+package blackjack;
+
+public class Test {
+
+	public static void main(String[] args){
+		Deck deck = new Deck();												//create new Deck object
+		
+		System.out.println("This is our deck!\n\n" + deck + "\n");			//Print the deck of cards
+		
+		deck.shuffle();														//shuffle deck
+		
+		System.out.println("This is our shuffled deck!\n\n" + deck + "\n");		//Print the shuffled deck
+		
+		Card myCard = deck.deal();											//deals a card
+		
+		System.out.println("We drew the " + myCard + "!");											//prints that card
+	}
+	
+}
