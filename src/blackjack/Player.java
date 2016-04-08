bb4ddb09a299638c0d6d2ea51f3623b612e7b834
mant1c0r3 <thelevimatusproject@gmail.com>
2016-04-08 10:15:42 -0300

    formatting and remove variable
    we weren't using status, so I got rid of it.

--- src/blackjack/Player.java
+++ src/blackjack/Player.java
@@ -2,7 +2,6 @@ package blackjack;
 
 public class Player {
 	//Shows players status (hit, stand, fold, bet)
-	private String status;
 	private Hand hand;
 	private Wallet wallet;
 	private String name;
@@ -13,15 +12,12 @@ public class Player {
 		wallet = new Wallet();
 	}
 	//Get methods:
-	public Hand getHand(){
-		return hand;
-	}
-	public Wallet getWallet(){
-		return wallet;
-	}
+	public Hand getHand(){return hand;}
+	public Wallet getWallet(){return wallet;}
+	public String getName(){return name;}
+	
 	//player decides to hit, new card gets added to players hand:
 	public void hit(Deck d){
-		status = "hit";
 		System.out.println(name + " has decided to hit.");
 		hand.addToHand(d.deal() );
 	}
@@ -32,13 +28,11 @@ public class Player {
 	
 	//player decides to stand:
 	public void stand(){
-		status = "stand";
 		System.out.println(name + " has decided to stand.");
 	}
 	//Player adds funds to the betting pool:
 	public void bet(double a, BettingPool b){
 		if (wallet.getTotalValue() >= a){
-			status = "bet";
 			wallet.removeFunds(a);
 			b.addMoney(a);
 		}
@@ -47,9 +41,7 @@ public class Player {
 	}
 	//Player decides to fold:
 	public void fold(){
-		status = "fold";
 		System.out.println(name + " has folded");
 	}
-	//Player quits, program terminates:
 
 }
